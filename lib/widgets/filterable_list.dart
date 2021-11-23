476a4473ea788ce36f78a7d2737c8d855d68c406
Eunice Vassoa <eunicevassoa@gmail.com>
2021-11-23 09:08:50 +0200

    refactor

--- lib/widgets/filterable_list.dart
+++ lib/widgets/filterable_list.dart
@@ -11,22 +11,31 @@ class FilterableList extends StatelessWidget {
     required this.search
   });
 
+  List<String> _getList() {
+    return items.where((element) {
+      return element.toLowerCase().contains(search.toLowerCase());
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.zero,
-        children: items.where((element) {
-          return element.toLowerCase().contains(search.toLowerCase());
-        }).toList().map<Widget>((e) {
-          return ListTile(
-            dense: true,
-            title: Text(e),
-            onTap: () => onItemTapped(e)
-          );
-        }).toList()
+    return Container(
+      constraints: BoxConstraints(
+        maxHeight: 100
+      ),
+      child: Visibility(
+        visible: _getList().isNotEmpty,
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.zero,
+          children: List.generate(_getList().length, (index) {
+            return ListTile(
+              dense: true,
+              title: Text(_getList()[index]),
+              onTap: () => onItemTapped(_getList()[index])
+            );
+          })
+        )
       )
     );
   }
